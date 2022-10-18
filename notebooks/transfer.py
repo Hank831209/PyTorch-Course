33d40de678700de3d638459379bacd3939e79437
Hank831209 <hanjhank.gmail.com>
2022-10-18 20:14:48 +0800

    update

--- notebooks/transfer.py
+++ notebooks/transfer.py
@@ -50,6 +50,7 @@ print(model.layer1[0].conv1.weight.requires_grad)  # False
 model = model.to(device)
 
 # optimizer
+# 需要算梯度的所有參數
 optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001, momentum=0.9)
 
 # loss
@@ -57,3 +58,45 @@ loss_fn = nn.CrossEntropyLoss()
 loss_fn = loss_fn.to(device)
 
 
+def train(model, dataloaders, loss_fn, optimizer, num_epochs=1):
+    best_acc = 0.
+    val_acc_history = list()
+    for epoch in range(num_epochs):
+        # train
+        model.train()
+        for inputs, labels in dataloaders['train']:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = loss_fn(outputs, labels)
+            preds = outputs.argmax(dim=1)  # data, label
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        #     running_loss += loss.item() * inputs.size(0)
+        #     running_corrects += torch.sum(preds.view(-1) == labels.view(-1)).item()
+        # epoch_loss = running_loss / len(dataloaders['train'].dataset)
+        # epoch_acc = running_corrects / len(dataloaders['train'].dataset)
+
+        # val
+        model.eval()
+        with torch.no_grad():
+            print('開始驗證')
+            running_loss = 0.
+            running_corrects = 0.
+            for inputs, labels in dataloaders['val']:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs) # bsize * 2
+                loss = loss_fn(outputs, labels) 
+                preds = outputs.argmax(dim=1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds.view(-1) == labels.view(-1)).item()
+            epoch_loss = running_loss / len(dataloaders['val'].dataset)
+            epoch_acc = running_corrects / len(dataloaders['val'].dataset)
+            print("Phase {} loss: {}, acc: {}".format('val', epoch_loss, epoch_acc))
+            val_acc_history.append(epoch_acc)
+            if epoch_acc > best_acc:
+                best_acc = epoch_acc
+                # torch.save(model.state_dict(), '123')
+
+
+train(model, dataloaders_dict, loss_fn, optimizer, num_epochs=5)
